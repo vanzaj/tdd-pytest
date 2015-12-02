8c86cda5d3d4e95589538aaa93a2b2311251ab3e
Ivan <vanzaj@gmail.com>
2015-12-02 17:48:39 +0800

    RDD + pytest setup

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,8 +1,48 @@
+import pytest
 from splinter import Browser
+
+
+@pytest.yield_fixture(scope='session')
+def browser():
+    b = Browser()
+    yield b
+    b.quit()
 
 
 BASE_URL = 'http://localhost:5000'
 
-with Browser() as browser:
-    browser.visit(BASE_URL)
+def _r(route):
+    if not route.startswith('/'):
+        route = '/' + route
+    return '%s%s' % (BASE_URL, route)
+
+
+# Edith has heard about a cool new online to-do app.
+# She goes to check out its homepage
+def test_can_check_homepage(browser):
+    browser.visit(_r('/'))
     assert browser.is_text_present('hello world')
+
+# She notices the page title and header mention to-do lists
+#assert 'To-Do' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
